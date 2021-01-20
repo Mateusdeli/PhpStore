60c1a0e2d13426c604bf12d58816c8c5f44a116d
Mateusdeli <mateus_forlevesi@hotmail.com>
2021-01-19 22:42:43 -0300

    Adicionado as configuracoes para a comunicacao com a database

--- config.php
+++ config.php
@@ -2,3 +2,9 @@
 
 define("APP_NAME", "PHPSTORE");
 define('APP_VERSION', 'v1.0.0');
+
+define('MYSQL_HOST', 'localhost');
+define('MYSQL_DATABASE', 'webstore');
+define('MYSQL_USER', 'mateus');
+define('MYSQL_PASSWORD', 'teste1');
+define('MYSQL_CHARSET', 'utf8');
