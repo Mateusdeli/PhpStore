a778c99d1b91837aeff67bcc52c8eff37278e3ae
Mateusdeli <mateus_forlevesi@hotmail.com>
2021-01-22 10:40:16 -0300

    Adicionado o controlador de rotas da aplicacao

---
+++ src/routes/routes.php
@@ -0,0 +1,28 @@
+<?php
+
+$routes = [
+    'home' => 'main@index',
+    'store' => 'main@index',
+    'cart' => 'store@cart',
+];
+
+$main_route = 'home';
+
+if (isset($_GET['a'])) {
+    if (!array_key_exists($_GET['a'], $routes)) {
+        $main_route = 'home';
+    }
+    else {
+        $main_route = $_GET['a'];
+    }
+}
+
+$class_and_action_route = explode('@', $routes[$main_route]);
+
+$controller_class = ucfirst($class_and_action_route[0]) . "Controller";
+$method_class = $class_and_action_route[1];
+
+$namespaceClass = "App\\WebStore\\Controllers\\".$controller_class;
+
+$class = new $namespaceClass();
+$class->$method_class();
