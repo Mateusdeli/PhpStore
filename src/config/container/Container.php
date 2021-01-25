92f9b43f2e5d744a9c47030a9a899ed2ee129f4a
Mateusdeli <mateus_forlevesi@hotmail.com>
2021-01-25 11:31:18 -0300

    Adicionado o container de injecao dependencias

---
+++ src/config/container/Container.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\WebStore\Config\Container;
+
+use App\WebStore\Classes\Functions;
+use App\WebStore\Classes\User;
+use App\WebStore\Classes\UserAuth;
+use DI\ContainerBuilder;
+
+class Container implements ContainerProvider
+{
+
+    private ContainerBuilder $container;
+
+    public function build()
+    {
+        $this->container = new ContainerBuilder();
+        $this->container->addDefinitions($this->definitions());
+        return $this->container->build();
+    }
+
+    private function definitions(): array
+    {
+        return [];
+    }
+}
---
+++ src/config/container/ContainerProvider.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\WebStore\Config\Container;
+
+use DI\ContainerBuilder;
+
+interface ContainerProvider
+{
+    public function build();
+}
