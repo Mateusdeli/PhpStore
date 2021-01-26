0233b7be822f52defc6b56b3a51058641e0f5404
Mateusdeli <mateus_forlevesi@hotmail.com>
2021-01-26 20:22:40 -0300

    ajuste no layout do header e footes

---
+++ src/views/layouts/header.php
@@ -0,0 +1,21 @@
+<?php use App\WebStore\Classes\Store;?>
+<div class="container-fluid h-nav">
+  <div class="row align-items-center pt-3 pb-3">
+      <div class="col-6 d-flex align-items-center">
+        <a href="/"><h3><?= APP_NAME ?></h3></a>
+      </div>
+      <div class="col-6 text-end">
+        <a href="?a=home">Home</a>
+        <a href="?a=store">Loja</a>
+          <?php if (Store::ClienteLogado()): ?>
+            <a href="#">Sair</a>
+          <?php else: ?>
+            <a href="#">Login</a>
+            <a href="#">Cadastrar</a>
+          <?php endif; ?>
+        <a href="#"><i class="fas fa-shopping-cart"></i>
+          <span class="badge bg-warning">10</span>
+        </a>
+      </div>
+  </div>
+</div>
