bba2d34d10049c847d6a1849446355c0860bac8d
Mateusdeli <mateus_forlevesi@hotmail.com>
2021-01-28 12:02:06 -0300

    adicionado o cliente biulder e o model cliente

--- src/models/Cliente.php
+++ src/models/Cliente.php
@@ -4,5 +4,83 @@ namespace App\WebStore\Models;
 
 class Cliente
 {
-    
+    private string $email;
+    private string $password;
+    private string $confirmPassword;
+    private string $nome_completo;
+    private string $endereco;
+    private string $cidade;
+    private string $telefone;
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function getConfirmPassword(): string
+    {
+        return $this->confirmPassword;
+    }
+
+    public function getNomeCompleto(): string
+    {
+        return $this->nome_completo;
+    }
+
+    public function getEndereco(): string
+    {
+        return $this->endereco;
+    }
+
+    public function getCidade(): string
+    {
+        return $this->cidade;
+    }
+
+    public function getTelefone(): string
+    {
+        return $this->telefone;
+    }
+
+    public function setEmail(string $email)
+    {
+        $this->email = $email;
+    }
+
+    public function setPassword(string $password)
+    {
+        $this->password = $password;
+    }
+
+    public function setConfirmPassword(string $confirmPassword)
+    {
+        $this->confirmPassword = $confirmPassword;
+    }
+
+    public function setNomeCompleto(string $nome_completo)
+    {
+        $this->nome_completo = $nome_completo;
+    }
+
+    public function setEndereco(string $endereco)
+    {
+        $this->endereco = $endereco;
+    }
+
+    public function setCidade(string $cidade)
+    {
+        $this->cidade = $cidade;
+    }
+
+    public function setTelefone(string $telefone)
+    {
+        $this->telefone = $telefone;
+    }
+
+
 }
--- src/models/builders/ClienteBuilder.php
+++ src/models/builders/ClienteBuilder.php
@@ -2,7 +2,62 @@
 
 namespace App\WebStore\Models\Builders;
 
+use App\WebStore\Models\Cliente;
+
 class ClienteBuilder
 {
+
+    private Cliente $cliente;
+
+    public function __construct() {
+        $this->cliente = new Cliente();
+    }
+
+    public function build(): Cliente
+    {
+        return $this->cliente;
+    }
     
+    public function hasEmail(string $email)
+    {
+        $this->cliente->setEmail($email);
+        return $this;
+    }
+
+    public function hasPassword(string $password)
+    {
+        $this->cliente->setPassword($password);
+        return $this;
+    }
+
+    public function hasConfirmPassword(string $confirmPassword)
+    {
+        $this->cliente->setConfirmPassword($confirmPassword);
+        return $this;
+    }
+
+    public function hasNomeCompleto(string $nomeCompleto)
+    {
+        $this->cliente->setNomeCompleto($nomeCompleto);
+        return $this;
+    }
+
+    public function hasEndereco(string $endereco)
+    {
+        $this->cliente->setEndereco($endereco);
+        return $this;
+    }
+
+    public function hasCidade(string $cidade)
+    {
+        $this->cliente->setCidade($cidade);
+        return $this;
+    }
+
+    public function hasTelefone(string $telefone)
+    {
+        $this->cliente->setTelefone($telefone);
+        return $this;
+    }
+
 }
