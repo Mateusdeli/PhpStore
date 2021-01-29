ad93a1a4b3ab4c1a1bb5eee4da652543aa70fbc8
Mateusdeli <mateus_forlevesi@hotmail.com>
2021-01-29 15:07:22 -0300

    configurado o container de DI

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -1,6 +1,14 @@
 <?php
 
+use App\WebStore\Config\Container as ConfigContainer;
+use App\WebStore\Config\Container\Container;
+use App\WebStore\Controllers\AuthController;
+use App\WebStore\Controllers\Services\AuthServices;
+use DI\Container as DIContainer;
+use DI\ContainerBuilder;
 use GuzzleHttp\Psr7\Request;
+
+use function PHPSTORM_META\type;
 
 $routes = [
     'home' => 'Home@index',
@@ -30,5 +38,8 @@ $method_class = $class_and_action_route[1];
 
 $namespaceClass = "App\\WebStore\\Controllers\\".$controller_class;
 
-$class = new $namespaceClass();
+$container = (new ConfigContainer())->buildContainer();
+
+$class = $container->get($namespaceClass);
 $class->$method_class();
+
