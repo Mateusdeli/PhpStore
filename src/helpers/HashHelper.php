b301e53e8cac2fadbe96cc97efd02708f5ba1116
Mateusdeli <mateus_forlevesi@hotmail.com>
2021-01-31 23:11:20 -0300

    adicionado a camada helper com helper gerador de hash

---
+++ src/helpers/HashHelper.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\WebStore\Helpers;
+
+class HashHelper
+{
+
+    public static function gerarHashAleatorio(int $num_caracteres = 12): string
+    {
+        $chars = "abcdefg@#hijklmn%opqrstuvxyzABCDEF@GHIJKLijklmnopqrMNOP5QRSTUVXYZSTU1$&2345";
+        return substr(str_shuffle($chars), 0, $num_caracteres);
+    }
+
+}
