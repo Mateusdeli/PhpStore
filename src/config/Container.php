9d31f9c6fecf221c2f6eca925e9d9356f6ae5172
Mateusdeli <mateus_forlevesi@hotmail.com>
2021-02-03 19:14:39 -0300

    adicionado a injection para enviar email

--- src/config/Container.php
+++ src/config/Container.php
@@ -5,6 +5,9 @@ namespace App\WebStore\Config;
 use App\WebStore\Classes\Database\Database;
 use App\WebStore\Controllers\AuthController;
 use App\WebStore\Services\AuthServices;
+use App\WebStore\Utils\Libs\Email\EmailInterface;
+use App\WebStore\Utils\Libs\Email\EmailSenderInterface;
+use App\WebStore\Utils\Libs\Email\PhpMailerAdapter;
 use App\WebStore\Utils\Libs\FormValidator\FormValidator;
 use App\WebStore\Utils\Libs\FormValidator\FormValidatorAdapterInterface;
 use App\WebStore\Utils\Libs\FormValidator\RakitFormValidator;
@@ -13,6 +16,7 @@ use App\WebStore\Utils\Libs\FormValidator\RespectValidator;
 use App\WebStore\Utils\Libs\FormValidator\ValitronFormValidationAdapter;
 use DI\Container as DIContainer;
 use DI\ContainerBuilder;
+use PHPMailer\PHPMailer\PHPMailer;
 use Rakit\Validation\Validator;
 
 use function DI\create;
@@ -32,7 +36,11 @@ class Container
        return [
             FormValidator::class => 
                 create(RakitFormValidatorAdapter::class)
-                    ->constructor(new Validator())
+                    ->constructor(new Validator()),
+
+            EmailSenderInterface::class => 
+                create(PhpMailerAdapter::class)
+                    ->constructor(new PHPMailer())
        ];
     }
 
