66ae610241d31e6a56cdaf71e1492a4a4c77e2e5
Mateusdeli <mateus_forlevesi@hotmail.com>
2021-02-03 19:13:50 -0300

    adicionado o helper para gerar hashs

--- src/helpers/HashHelper.php
+++ src/helpers/HashHelper.php
@@ -7,7 +7,7 @@ class HashHelper
 
     public static function gerarHashAleatorio(int $num_caracteres = 12): string
     {
-        $chars = "abcdefg@#hijklmn%opqrstuvxyzABCDEF@GHIJKLijklmnopqrMNOP5QRSTUVXYZSTU1$&2345";
+        $chars = "abcdefg@#hijklmn%opqrstuvxyzABCDEF@GHIJKLijklmnopqrMNOP5QRSTUVXYZSTU1$2345";
         return substr(str_shuffle($chars), 0, $num_caracteres);
     }
 
