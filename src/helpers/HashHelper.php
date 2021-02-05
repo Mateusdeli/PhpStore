b4abfd3a84c25c2066f9a3e5386e0aceb5eb740d
Mateusdeli <mateus_forlevesi@hotmail.com>
2021-02-04 21:49:11 -0300

    aumentado o tamanho do token

--- src/helpers/HashHelper.php
+++ src/helpers/HashHelper.php
@@ -5,7 +5,7 @@ namespace App\WebStore\Helpers;
 class HashHelper
 {
 
-    public static function gerarHashAleatorio(int $num_caracteres = 12): string
+    public static function gerarHashAleatorio(int $num_caracteres = 40): string
     {
         $chars = "abcdefg@#hijklmn%opqrstuvxyzABCDEF@GHIJKLijklmnopqrMNOP5QRSTUVXYZSTU1$2345";
         return substr(str_shuffle($chars), 0, $num_caracteres);
