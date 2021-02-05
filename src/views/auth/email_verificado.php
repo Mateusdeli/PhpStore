9ce1d415395fa16024518d4022ce936a64585045
Mateusdeli <mateus_forlevesi@hotmail.com>
2021-02-04 22:23:28 -0300

    adicionado a view de validacao do email

---
+++ src/views/auth/email_verificado.php
@@ -0,0 +1,7 @@
+<div class="container">
+    <?php if (isset($mensagem_success)): ?>
+        <div class="alert alert-success" role="alert">
+            <p><?= $mensagem_success ?></p>
+        </div>
+    <?php endif; ?>
+</div>
