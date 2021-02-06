59bbc6271a9bdc45b783dba602e769719e57c421
Mateusdeli <mateus_forlevesi@hotmail.com>
2021-02-05 23:53:45 -0300

    adicionado a view do email verificado

--- src/views/auth/email_verificado.php
+++ src/views/auth/email_verificado.php
@@ -1,7 +1,9 @@
 <div class="container">
-    <?php if (isset($mensagem_success)): ?>
-        <div class="alert alert-success" role="alert">
-            <p><?= $mensagem_success ?></p>
-        </div>
-    <?php endif; ?>
+    <div class="row">
+      <div class="col-sm-6 offset-sm-3 text-center">
+        <h3 class="my-5">Conta confimada com sucesso!</h3>
+        <p>A sua conta de cliente foi confimada. Seja bem-vindo(a) a nossa loja.</p>
+        <a href="?a=login_form" class="btn btn-primary">Login</a>
+      </div>
+    </div>
 </div>
