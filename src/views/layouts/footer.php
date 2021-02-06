e381871fcc7300a483d3d872318bbb4418a9e7c4
Mateusdeli <mateus_forlevesi@hotmail.com>
2021-02-05 23:52:24 -0300

    ajuste no rodape

--- src/views/layouts/footer.php
+++ src/views/layouts/footer.php
@@ -1,7 +1,7 @@
 <div class="container-fluid h-footer fixed-bottom">
     <div class="row">
         <div class="col-12 text-center p-3">
-          <?= $_ENV['APP_NAME'] . '('.$_ENV['APP_VERSION'].') &copy; ' . date('Y') ?>
+          <?= $_ENV['APP_NAME'] . ' ('.$_ENV['APP_VERSION'].') &copy; ' . date('Y') ?>
         </div>
     </div>
 </div>
