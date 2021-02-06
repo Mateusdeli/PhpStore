b76ff8f998caa37da0ec02f64ddd9963e60eaba7
Mateusdeli <mateus_forlevesi@hotmail.com>
2021-02-05 23:51:42 -0300

    adicionado a view da conta criada com sucesso

---
+++ src/views/auth/create_account_success.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <div class="row">
+      <div class="col-sm-6 offset-sm-3 text-center">
+        <h3 class="my-5">Conta criada com sucesso!</h3>
+        <p>Foi enviado um email com o link de confirmação da sua conta.</p>
+        <p>Pro favor, verifique se o email aparece em sua conta ou se foi movido para a pasta de SPAM.</p>
+        <a href="?a=home" class="btn btn-primary">Voltar</a>
+      </div>
+    </div>
+</div>
