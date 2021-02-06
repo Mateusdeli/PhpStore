b323f7d1695a2c81f4d4f37a1093290f9ff180fb
Mateusdeli <mateus_forlevesi@hotmail.com>
2021-02-05 23:51:24 -0300

    adicionado a view do formulario de login

---
+++ src/views/auth/login_form.php
@@ -0,0 +1,53 @@
+<div class="container">
+    <div class="row">
+      <div class="col-sm-6 offset-sm-3">
+        <h3 class="text-center my-5">Login</h3>
+
+        <?php if (isset($_SESSION['error'])): ?>
+            <div class="alert alert-danger text-center p-2">
+                <?php if(is_array($_SESSION['error'])): ?>
+                    <ul class="list-errors">
+                        <?php foreach ($_SESSION['error'] as $error): ?>
+                            <?php foreach ($error as $err): ?>
+                                <li><?= $err ?></li>
+                            <?php endforeach; ?>
+                        <?php endforeach; ?>
+                    </ul>
+                <?php else: ?>
+                    <span><?= $_SESSION['error'] ?></span>
+                <?php endif; ?>
+            </div>
+            <?php unset($_SESSION['error']) ?>
+        <?php endif; ?>
+
+        <?php if (isset($conta_criada) || !empty($conta_criada)): ?>
+            <div class="alert alert-success text-center p-2">
+                <span><?= $conta_criada ?></span>
+            </div>
+        <?php endif; ?>
+
+        <?php if (isset($msg_email_confirmado) || !empty($msg_email_confirmado)): ?>
+            <div class="alert alert-success text-center p-2">
+                <span><?= $msg_email_confirmado ?></span>
+            </div>
+        <?php endif; ?>
+
+        <form action="?a=login" method="post">
+            <div class="my-3">
+                <label for="text_email">Email</label>
+                <input type="text" name="text_email" id="text_email" placeholder="Email" class="form-control">
+            </div>
+
+            <div class="my-3">
+                <label for="text_senha_1">Senha</label>
+                <input type="password" name="text_senha_1" id="text_senha_1" placeholder="Senha" class="form-control" required>
+            </div>
+
+            <div class="my-4">
+                <button type="submit" class="btn btn-primary">Entrar</button>
+            </div>
+        </form>
+       
+      </div>
+    </div>
+</div>
